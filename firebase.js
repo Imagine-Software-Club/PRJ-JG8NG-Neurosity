eea17a0b55d15102122a4222b1335f992cd1a1cc
Minh Pham <phamtuannhatminh@gmail.com>
2023-10-19 18:04:33 -0400

    add firebase config

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB03Et0W-Xwpa2OO1d65ahF5SH_zVdjPDg",
+  authDomain: "neurosity-crown.firebaseapp.com",
+  projectId: "neurosity-crown",
+  storageBucket: "neurosity-crown.appspot.com",
+  messagingSenderId: "177033816422",
+  appId: "1:177033816422:web:11221f9e46ccd227d1c0a7"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
