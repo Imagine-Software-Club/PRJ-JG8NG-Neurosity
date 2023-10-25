ba39066f4a6c9f22af72cceef616e45d3c75a8e9
Nathanael Lu <lunathanael@gmail.com>
2023-10-25 17:53:23 -0400

    added some buttons, exploring Firebase AuthUI

--- app/(tabs)/login.tsx
+++ app/(tabs)/login.tsx
@@ -3,8 +3,13 @@ import {
     Text, 
     ImageBackground,
     Button,
-    StyleSheet
+    StyleSheet,
+    Pressable
 } from 'react-native'
+
+import firebase from 'firebase/compat/app';
+import * as firebaseui from 'firebaseui'
+import 'firebaseui/dist/firebaseui.css'
 
 
 const textFont = 'Arial'
@@ -17,6 +22,12 @@ export default function LoginScreen() {
                 <Text style={styles.title}>Neurosity Crown</Text>
             </View>
             <View>
+                <Button style={styles.button} title="Login With Email"></Button>
+                <Button style={styles.button} title="Twitter"></Button>
+                <Button style={styles.button} title="GitHub"></Button>
+                <Button style={styles.button} title="X"></Button>
+                <Button style={styles.button} title="Google"></Button>
+                <Button style={styles.button} title="Facebook"></Button>
                 {/* <Button></Button> */}
             </View>
         </View>
@@ -31,12 +42,9 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  image: {
-    flex: 1,
-    justifyContent: 'center',
-  },
   title: {
-    fontSize: 48,
+    flex: 0,
+    fontSize: 48, // Subject to change
     alignItems: 'center',
     fontWeight: 'bold',
     color: '#FEFAE0',
