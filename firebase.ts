9b29d5b86f51045b1e26e357aee5d2d8b9f08fd4
Minh Pham <phamtuannhatminh@gmail.com>
2023-11-02 18:32:12 -0400

    check if firebase app is already initialized

--- firebase.ts
+++ firebase.ts
@@ -1,23 +1,30 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
+import firebase, { initializeApp, getApp } from "firebase/app";
+import 'firebase/auth'
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY, 
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderIFd: process.env.FIREBASE_MESSAGING_SENDER_IFD,
-  appId: process.env.FIREBASE_APP_ID
-};
+let app : firebase.FirebaseApp
+try {
+  app = getApp()
+} catch(error) {
+  const firebaseConfig = {
+    apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY, 
+    authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID, 
+    storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderIFd: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_IFD,
+    appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID
+  };
+  app = initializeApp(firebaseConfig)
 
+}
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-// const auth = getAuth(app);
+
+
 // const provider = new GoogleAuthProvider();
 
-export default app;
+export default app
 // export {auth, provider};
