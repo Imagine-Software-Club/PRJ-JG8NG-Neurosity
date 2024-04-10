3f8ea195b974f9474a6a8f44c2e6f4bee72d9771
Ken Pham <phammin5@msu.edu>
2024-04-10 13:56:42 -0400

    Add documentation

--- python_scripts/youtube_clip_generator.py
+++ python_scripts/youtube_clip_generator.py
@@ -3,14 +3,22 @@ from moviepy.editor import VideoFileClip
 
 
 def download_video(youtube_url, output_filename):
+    # Create a YouTube object using the provided URL, with OAuth authentication enabled
     yt = YouTube(youtube_url, use_oauth=True, allow_oauth_cache=True)
+
+    # Filter and retrieve all progressive streams (streams that contain both video and audio)
     yt_stream = yt.streams.filter(progressive=True, file_extension='mp4').all()
+
+    # Download the highest quality stream and save it with the specified filename
     yt_stream[-1].download(filename=output_filename)
 
 
 def extract_clip_from_video(start_time, end_time, filename):
     clip = VideoFileClip(filename)
+
+    # Extract a subclip from the specified start time to end time
     extract = clip.subclip(start_time, end_time)
+    
     extract.write_videofile("result.mp4")
 
     clip.close()
