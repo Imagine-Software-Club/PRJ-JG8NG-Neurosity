2dbc24875981f3a3fc9757215ab05f4f5b03f63b
Ken Pham <phammin5@msu.edu>
2024-04-17 12:16:48 -0400

    Add comments about the date used for cleaning timestamps

--- python_scripts/master_script.py
+++ python_scripts/master_script.py
@@ -6,14 +6,14 @@ def main():
     # starting_time = input("Enter date and time: ")
 
     movie_path = "../js/basics/data/Doctor Who - S 3 E 10 - Blink.mp4"
-    file_path = "../js/basics/data/doctor_who_s3e10_focus.csv"
-    starting_time = "2024-04-10 04:56:38"
+    file_path = "../js/basics/data/drwhos3e10_ken_focus.csv"
+    starting_time = "2024-04-17 19:46:43" # remember to change the date to today's date or else the script won't run
 
     print("Cleaning data...")
     cleaned_csv_path = subprocess.check_output(["python", "clean_timestamp.py", file_path, starting_time]).strip()
 
     print("Generating timestamps...")
-    timestamps_list = subprocess.check_output(["python", "timestamp_generator.py", cleaned_csv_path]).decode().strip().split("\r\n")
+    timestamps_list = subprocess.check_output(["python", "timestamp_generator_count.py", cleaned_csv_path]).decode().strip().split("\r\n")
 
     print("Extracting clips...")
     for timestamps in timestamps_list:
